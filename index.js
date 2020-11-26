981427b9cde4a0f0728acb64a4043e9705ebf42e
brianoconnell98 <117376663@umail.ucc.ie>
2020-11-26 17:32:01 +0000

    Added CORS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ patientsRouter = require("./api/patients")
 physiosRouter = require("./api/physios")
 
 app.use(express.json())
-
+app.use(cors())
 app.get("/", (req, res) =>{
     res.json({
         message: "Hello World!"
