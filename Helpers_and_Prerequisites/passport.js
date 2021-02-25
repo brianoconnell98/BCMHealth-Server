436ba568fa02ab0de913dd3fa9f5f6155acd0128
brianoconnell98 <117376663@umail.ucc.ie>
2021-02-25 00:27:22 +0000

    Iteration 4.4 passport

--- Helpers_and_Prerequisites/passport.js
+++ Helpers_and_Prerequisites/passport.js
@@ -8,21 +8,21 @@ import {Patient} from '../DB/Models/patient.js';
 import {Physio} from '../DB/Models/physio.js'
 
 // // Nathaniel woodbury version of passport
-// const instantiate = function(passport) {
-//     passport.use(
-//         new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-//             Patient.findOne({ email: email}, (err,patient) => {
-//                 if (err) throw err;
-//                 if (!patient) return done(null, false, { message: 'The email provided is not registered'});
-//                 bcrypt.compare(password, patient.password, (err, result) => {
-//                     if (err) throw err;
-//                     if (result === true) {
-//                         return done(null, patient);
-//                     } else {
-//                         return done(null, false, { message: 'The password is incorrect' });
-//                     }
-//                 });
-//             })
+const instantiate = function(passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+            Patient.findOne({ email: email}, (err,patient) => {
+                if (err) throw err;
+                if (!patient) return done(null, false, { message: 'The email provided is not registered'});
+                bcrypt.compare(password, patient.password, (err, result) => {
+                    if (err) throw err;
+                    if (result === true) {
+                        return done(null, patient);
+                    } else {
+                        return done(null, false, { message: 'The password is incorrect' });
+                    }
+                });
+            })
                
     //                Physio.findOne({ email: email}, (err,physio) => {
     //                 if (err) throw err;
@@ -37,47 +37,47 @@ import {Physio} from '../DB/Models/physio.js'
     //                 });
     //             });
 
-        const instantiate = function(passport) {
-            passport.use(
-                new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-                    // Match Patient
-                    Patient.findOne({ email: email })
-                    .then(patient => {
-                        if(!patient) {
-                            return done(null, false, { message: 'That email is not registered' });
-                        }
+    //     const instantiate = function(passport) {
+    //         passport.use(
+    //             new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    //                 // Match Patient
+    //                 Patient.findOne({ email: email })
+    //                 .then(patient => {
+    //                     if(!patient) {
+    //                         return done(null, false, { message: 'That email is not registered' });
+    //                     }
 
-                // Match Password
-                bcrypt.compare(password, patient.password, (err, isMatch) => {
-                    if(err) throw err;
+    //             // Match Password
+    //             bcrypt.compare(password, patient.password, (err, isMatch) => {
+    //                 if(err) throw err;
 
-                    if(isMatch) {
-                        return done(null, patient);
-                    } else {
-                        return done(null, false, { message: 'Password is incorrect' });
-                    }
-                });
-            })
-            .catch(err => console.log(err));
+    //                 if(isMatch) {
+    //                     return done(null, patient);
+    //                 } else {
+    //                     return done(null, false, { message: 'Password is incorrect' });
+    //                 }
+    //             });
+    //         })
+    //         .catch(err => console.log(err));
         
-            // Match Physio
-            Physio.findOne({ email: email })
-            .then(physio => {
-                if(!physio) {
-                    return done(null, false, { message: 'That email is not registered' });
-                }
+    //         // Match Physio
+    //         Physio.findOne({ email: email })
+    //         .then(physio => {
+    //             if(!physio) {
+    //                 return done(null, false, { message: 'That email is not registered' });
+    //             }
 
-                // Match Password
-                bcrypt.compare(password, physio.password, (err, isMatch) => {
-                    if(err) throw err;
+    //             // Match Password
+    //             bcrypt.compare(password, physio.password, (err, isMatch) => {
+    //                 if(err) throw err;
 
-                    if(isMatch) {
-                        return done(null, physio);
-                    } else {
-                        return done(null, false, { message: 'Password is incorrect' });
-                    }
-                });
-            })
+    //                 if(isMatch) {
+    //                     return done(null, physio);
+    //                 } else {
+    //                     return done(null, false, { message: 'Password is incorrect' });
+    //                 }
+    //             });
+    //         })
             .catch(err => console.log(err));
         })
     );
